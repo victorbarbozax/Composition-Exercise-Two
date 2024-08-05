406a4dceb21002979828a666edacf112bae46983
Victor Barboza <victor.barboza052008@gmail.com>
2024-08-05 17:10:19 -0300

    Primeiro commit do exercicio de cliente

---
+++ Exercises-Section13/src/entities/OrderItem.java
@@ -0,0 +1,67 @@
+package entities;
+
+public class OrderItem {
+
+	private Integer quantity;
+	private Double price;
+	
+	private Product product;
+	
+	public OrderItem() {
+	}
+
+	public OrderItem(Integer quantity, Double price, Product product) {
+		super();
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+    public double subTotal() {
+           return price * quantity;
+    }
+
+    @Override
+    public String toString() {
+    	return product.getName() 
+    			
+    			//TV, $1000.00, Quantity: 1, Subtotal: $1000.00
+    			//Mouse, $40.00, Quantity: 2, Subtotal: $80.00
+    			
+    			+ ", $ "
+    			
+    			+ String.format("%.2f ", price)
+    			
+    	        + " Quantity: "
+                + quantity
+                
+                + ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
+    
+}
+
---
+++ Exercises-Section13/src/entities/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package entities.enums;
+
+public enum OrderStatus {
+
+	PENDIND_PAYEMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+	
+	
+}
